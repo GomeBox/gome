3b16611b07ea0cf852bf1acda27c155945926ebb
Flotto85 <flohotto@gmx.de>
2020-10-22 17:20:39 +0200

    Fix: Fullscreen not working resolves #4

--- internal/game/settings.go
+++ internal/game/settings.go
@@ -7,18 +7,18 @@ import (
 
 //Settings contains all settings to run a game
 type settings struct {
-	windowSettings graphics.WindowSettings
+	windowSettings *graphics.WindowSettings
 }
 
 //WindowSettings returns the settings of the game window
 func (s settings) WindowSettings() *graphics.WindowSettings {
-	return &s.windowSettings
+	return s.windowSettings
 }
 
 //NewSettings initializes new Settings with default values
 func NewSettings() game.Settings {
 	s := new(settings)
-	s.windowSettings = *new(graphics.WindowSettings)
+	s.windowSettings = new(graphics.WindowSettings)
 	s.windowSettings.Resolution.Width = 800
 	s.windowSettings.Resolution.Height = 600
 	return *s
