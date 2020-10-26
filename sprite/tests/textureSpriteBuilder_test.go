d7030735de5f76df9692d1cf4fbe2e84c3e4bc63
Flotto85 <flohotto@gmx.de>
2020-10-26 17:28:10 +0100

    Added Test for TextSpriteBuilder Added Test for TextureSpriteBuilder

---
+++ sprite/tests/textureSpriteBuilder_test.go
@@ -0,0 +1,48 @@
+package tests
+
+import (
+	"github.com/GomeBox/gome/graphics/mocks"
+	"github.com/GomeBox/gome/primitives"
+	"github.com/GomeBox/gome/sprite"
+	"testing"
+)
+
+func TestFromTexture_Get(t *testing.T) {
+	textureMock := new(mocks.Texture)
+	s := sprite.FromTexture(textureMock).Get()
+	if s == nil {
+		t.Error("Sprite not properly created")
+	}
+}
+
+func TestFromTexture_SetPosition(t *testing.T) {
+	textureMock := new(mocks.Texture)
+	wantX := float32(123.24)
+	wantY := float32(234.56)
+	s := sprite.FromTexture(textureMock).SetPosition(wantX, wantY).Get()
+	gotX := s.Position().X
+	gotY := s.Position().Y
+	if wantY != gotY {
+		t.Errorf("Position not set (Y): got: %f, want %f", gotY, wantY)
+	}
+	if wantX != gotX {
+		t.Errorf("Position not set (X): got: %f, want %f", gotX, wantX)
+	}
+}
+
+func TestFromTexture_DrawerProperlySet(t *testing.T) {
+	textureMock := new(mocks.Texture)
+	drawCalled := false
+	textureMock.OnDrawF = func(source *primitives.Rectangle, dest *primitives.RectangleF) error {
+		drawCalled = true
+		return nil
+	}
+	s := sprite.FromTexture(textureMock).Get()
+	err := s.Draw()
+	if err != nil {
+		t.Errorf("err was expected to be nil but was %v", err)
+	}
+	if !drawCalled {
+		t.Error("Texture.DrawF was not called")
+	}
+}
