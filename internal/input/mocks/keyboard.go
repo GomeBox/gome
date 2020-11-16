5578e194316119098c20ccddd5cc94f17e803894
Flotto85 <flohotto@gmx.de>
2020-11-16 08:14:58 +0100

    renamed keyboard mock

--- internal/input/mocks/keyboard.go
+++ internal/input/mocks/keyboard.go
@@ -5,26 +5,26 @@ import (
 	"github.com/GomeBox/gome/primitives"
 )
 
-type KeyboardMock struct {
+type Keyboard struct {
 	CallCntUpdate   int
 	OnRegisterKey   func(keyType primitives.KeyType) (interfaces.Key, error)
 	OnUnregisterKey func(keyType primitives.KeyType)
 }
 
-func (k *KeyboardMock) RegisterKey(keyType primitives.KeyType) (interfaces.Key, error) {
+func (k *Keyboard) RegisterKey(keyType primitives.KeyType) (interfaces.Key, error) {
 	if k.OnRegisterKey != nil {
 		return k.OnRegisterKey(keyType)
 	}
 	return nil, nil
 }
 
-func (k *KeyboardMock) UnregisterKey(keyType primitives.KeyType) {
+func (k *Keyboard) UnregisterKey(keyType primitives.KeyType) {
 	if k.OnUnregisterKey != nil {
 		k.OnUnregisterKey(keyType)
 	}
 }
 
-func (k *KeyboardMock) Update() error {
+func (k *Keyboard) Update() error {
 	k.CallCntUpdate++
 	return nil
 }
